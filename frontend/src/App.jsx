a0ac2a87ad70b2695c3e5c83872584652c938174
Luke <kadalski.lukasz@gmail.com>
2025-12-04 12:10:06 +0100

    Provide URL for backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import { useEffect, useState } from "react";
 
-const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:3001";
-
 function App() {
   const [message, setMessage] = useState("Loading...");
 
   useEffect(() => {
-    fetch(`${backendUrl}/api/hello`)
+    fetch(`https://backend-248026660020.europe-central2.run.app/api/hello`)
       .then(r => r.json())
       .then(d => setMessage(d.message))
       .catch(() => setMessage("Error"));
