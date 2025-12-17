bb2851242f9383dadc601e43c7a1e6288f95e230
Luke <kadalski.lukasz@gmail.com>
2025-12-17 06:03:05 +0100

    AWS ECS WIP

--- frontend/server.js
+++ frontend/server.js
@@ -1,6 +1,6 @@
 // frontend/server.js
 import express from "express";
-import { GoogleAuth } from "google-auth-library";
+// import { GoogleAuth } from "google-auth-library";
 
 const backendUrl = process.env.VITE_BACKEND_URL || "http://localhost:3001";
 const app = express();
@@ -10,10 +10,11 @@ app.use(express.static("dist")); // Vite build output
 
 app.get("/api/proxy", async (req, res) => {
   try {
-    const auth = new GoogleAuth();
-    const client = await auth.getIdTokenClient(backendUrl);
+    // const auth = new GoogleAuth();
+    // const client = await auth.getIdTokenClient(backendUrl);
 
-    const response = await client.request({ url: `${backendUrl}/api/hello` });
+    // const response = await client.request({ url: `${backendUrl}/api/hello` });
+    const response = await fetch(`${backendUrl}/api/hello`);
     res.json(response.data);
   } catch (err) {
     console.error(err);
